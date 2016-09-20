d9cb44490f4f2f970a5ea074f28626f3d14a94d8
Douglas Wassarman <student@otp587740ots.ucsf.edu>
2016-09-20 16:26:07 -0700

    doug update

--- doug_script.py
+++ doug_script.py
@@ -5,8 +5,6 @@ import numpy as np
 # key: barcode sequence   value: (codon number, codon)
 allele_dic = pickle.load(open('allele_dic_with_WT.pkl','rb'))
 
-print allele_dic
-
 # key: amino acid   value: number
 amino_to_number = pickle.load(open('aminotonumber.pkl', 'rb'))
 
@@ -35,12 +33,13 @@ for i in range(len(ub)):
 plt.figure(figsize=(12,6))
 plt.imshow(a, interpolation='nearest', aspect='auto')
 plt.colorbar()
-column_labels = range(77)
 rows = ["" for k in range(21)]
 for amino in amino_to_number:
 	rows[amino_to_number[amino]] = amino
-row_labels = rows
-plt.yticks(range(21), row_labels)
+
+plt.yticks(range(21), rows)
+plt.xlabel('residue #')
+plt.ylabel('residue identity')
 plt.savefig('doug_figure.png', bbox_inches='tight')
 
 pickle.dump(a, open('doug_array.pkl', 'w'))
