20e2a4f1b0f72e536dbb56b72fc48f67f0ecd626
Greyson Lewis <greyson.lewis@ucsf.edu>
2016-10-17 21:38:01 -0700

    Added hamming distance calculation to pull sequences with at most one character difference from our indices

--- multiplex.py
+++ multiplex.py
@@ -1,23 +1,32 @@
 import gzip
 import shutil
 
-file = "ex.fastq.gz"
+file = "Undetermined_S0_R1_001.fastq.gz"
 
 indices = {"TTGACT", "GGAACT", "TGACAT", "GGACGG", "CTCTAC", "GCGGAC"}
 
 indexed_file = open("indexed.fastq", "w")
 
+
+def hamming_dist(s1, s2): 
+	assert len(s1) == len(s2)
+	return sum(c1 != c2 for c1, c2 in zip(s1, s2))
+
 with gzip.open(file, "rb") as f: #need gzip.open for fastq file
 	count = -1
 	for line in f:
-		print line
+		# print line
 		count += 1
-		if (count % 4 == 0) and (line[45:51] in indices) == True:
-			indexed_file.write(line)
-			indexed_file.write(f.next())
-			indexed_file.write(f.next())
-			indexed_file.write(f.next())
-			count += 3
+		if (count % 4 == 0):
+			close = False
+			for ind in indices:
+				close = close or (hamming_dist(line[45:51], ind) < 2)
+			if close:
+				indexed_file.write(line)
+				indexed_file.write(f.next())
+				indexed_file.write(f.next())
+				indexed_file.write(f.next())
+				count += 3
 
 with open('indexed.fastq', 'rb') as f_in, gzip.open('indexed.fastq.gz', 'wb') as f_out:
     f_out.write(f_in.read())
